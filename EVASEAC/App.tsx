0b3a48cb494d6e69a21fad5fdc69e06f37f49385
edoommm <emendozam1501@alumno.ipn.mx>
2022-05-22 14:52:46 -0500

    added screens
    see: https://reactnative.dev/docs/navigation

--- EVASEAC/App.tsx
+++ EVASEAC/App.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>SplashScreen/LogIn screen</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='LogIn'
+          component={LogInScreen}
+        />
+        <Stack.Screen
+          name='SignUp'
+          component={SignUpScreen}
+        />
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -22,3 +35,53 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+type RootStackParamList = {
+
+  Pdp: undefined; //current screen
+
+  SignUp: undefined; // don't pass any props to it
+
+  Home: undefined;
+
+  // see: https://stackoverflow.com/questions/47924501/add-strong-typing-for-react-navigation-props
+};
+
+interface NavProps {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'Pdp'>;
+}
+
+const LogInScreen = (props: NavProps) => {
+  return (
+    <View style={styles.container}>
+      <Text>SplashScreen/LogIn screen</Text>
+      <StatusBar style="auto" />
+      <Button
+        title='Sign Up'
+        onPress={() => props.navigation.navigate('SignUp')}
+      />
+      <Button
+        title='Home'
+        onPress={() => props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+};
+
+const SignUpScreen = (props: NavProps) => {
+  return (
+    <View style={styles.container}>
+      <Text>Sign Up screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const HomeScreen = (props: NavProps) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home screen</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
