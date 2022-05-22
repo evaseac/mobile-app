cef8ec7991387e35a799381946b65e2f69de47aa
edoommm <emendozam1501@alumno.ipn.mx>
2022-05-22 14:20:32 -0500

    React Navigation module and dependencies installed

--- EVASEAC/App.tsx
+++ EVASEAC/App.tsx
@@ -1,12 +1,16 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SplashScreen/LogIn screen</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>SplashScreen/LogIn screen</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
