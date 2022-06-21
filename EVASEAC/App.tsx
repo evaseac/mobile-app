24824f2204c6fa32baef3c49fd932ccaaae91543
edoommm <emendozam1501@alumno.ipn.mx>
2022-06-20 20:06:27 -0500

    modified project structure

--- EVASEAC/App.tsx
+++ EVASEAC/App.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LogInScreen from './components/LogIn';
-import SignUpScreen from './components/SignUp';
-import HomeScreen from './components/Home';
+import LogInScreen from './screens/LogInScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
---
+++ EVASEAC/types.tsx
@@ -0,0 +1,9 @@
+// TODO: declare RootStackParamList and implement this file and related work
+
+// ...
+    export type RootStackParamList = {
+        Modal: undefined;
+        NotFound: undefined;
+        Login: undefined;
+    };
+// ...
