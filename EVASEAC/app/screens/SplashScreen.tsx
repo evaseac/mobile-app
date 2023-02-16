5f9ada4e91399c554d9c34a9b612f9dfb7e1e32e
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2023-02-15 20:17:03 -0600

    Create first version of Splash Screen
    Create first version of Splash Screen and add functionality to jump to
    the next screen when a certain amount of time has passed. Also add
    images used in Splash Screen to assets directory.

---
+++ EVASEAC/app/screens/SplashScreen.tsx
@@ -0,0 +1,70 @@
+import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
+import React, { useEffect } from 'react';
+import Constants from 'expo-constants';
+
+interface SplashScreenProps {
+    navigation: any;
+}
+
+const SplashScreen = (props : SplashScreenProps) => {
+    useEffect(() => {
+        setTimeout(() => {
+            props.navigation.navigate('login');
+        }, 2000)
+    }, [props.navigation]);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={require('../../assets/ipn.png')}
+                />
+                <Image
+                    style={styles.image}
+                    source={require('../../assets/encb.png')}
+                />
+                <Image
+                    style={styles.image}
+                    source={require('../../assets/logo_concurso.png')}
+                />
+                <Image
+                    style={styles.image}
+                    source={require('../../assets/escom.png')}
+                />
+            </View>
+            <Text style={styles.text}>Evaseac 1.0</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: "#ffff",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    image: {
+        flex: 1,
+        margin: 10,
+        resizeMode: "center",
+    },
+
+    imageContainer: {
+       marginTop: Constants.statusBarHeight,
+       justifyContent: "center",
+       alignItems: "center",
+       height: "70%",
+    },
+
+    text: {
+        color: "#9c9696",
+        fontSize: 30,
+    },
+});
+
+
+export default SplashScreen;
