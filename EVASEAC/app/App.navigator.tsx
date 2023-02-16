74a4b5162f5c342c19aca02a32d40f8e617fd58c
Esteban Raymundo Olmedo Ramírez <43709368+EstebanOlmedo@users.noreply.github.com>
2023-02-16 10:56:13 -0600

    Implement Start Screens (#7) (#15)
    * Implement first version of Login Signup screens
    
    Create a first version of Login and Signup screens based on mock
    images. Change the default screen to login screen and also create fonts
    directory and add Helvetica font. Some possible improvements are:
    
      - In `SignUp` screen the title is static, and if more inputs are added
        then they will overlap with the title
      - The same applies to `Login` screen but with the footer.
    
    * Create first version of Splash Screen
    
    Create first version of Splash Screen and add functionality to jump to
    the next screen when a certain amount of time has passed. Also add
    images used in Splash Screen to assets directory.
    
    * Change initial screen from Login to Splash
    
    * Erase unused files
    
    * Add more fields to the register form
    
    * Prevent user from going back to splash screen

--- EVASEAC/app/App.navigator.tsx
+++ EVASEAC/app/App.navigator.tsx
@@ -14,17 +14,17 @@ import SitesScreen from "./screens/sites/SitesScreen";
 import CreateSitesScreen from "./screens/sites/CreateSitesScreen";
 import Protocol3Screen from "./screens/protocol3/Protocol3Screen";
 import Protocol4Screen from "./screens/protocol4/Protocol4Screen";
+import SplashScreen from "./screens/SplashScreen";
 
 const AppNavigator = () => (
     <NavigationContainer>
-        <Navigator 
-            screenOptions={{
-                headerShown : false
-            }} initialRouteName="login"
+        <Navigator
+            initialRouteName="splash"
         >
-            <Screen name="signup" component={SignUpScreen}></Screen>
+            <Screen name="splash" component={SplashScreen} options={{ headerShown : false }}></Screen>
+            <Screen name="login" component={LoginScreen} options={{ headerShown : false }}></Screen>
             <Screen name="home" component={HomeScreen}></Screen>
-            <Screen name="login" component={LoginScreen}></Screen>
+            <Screen name="signup" component={SignUpScreen} options={{ title: 'Registro' }}></Screen>
             <Screen name="Protocol1" component={Protocol1Screen}></Screen>
             <Screen name="Protocol2" component={Protocol2MainScreen}></Screen>
             <Screen name="Protocol3" component={Protocol3Screen}></Screen>
--- EVASEAC/app/screens/LoginScreen.tsx
+++ EVASEAC/app/screens/LoginScreen.tsx
@@ -1,32 +1,139 @@
-import { useLinkProps } from "@react-navigation/native";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View, Image, TextInput, ImageBackground, BackHandler } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
 
 interface LoginScreenProps {
     navigation : any;
 }
 
 const LoginScreen = (props : LoginScreenProps) => {
-    const signUp = () => {
-       props.navigation.navigate("signup") 
-    } 
-    const login = () => {
+    const [loaded] = useFonts({
+        Helvetica : require("../../assets/fonts/Helvetica-Bold.ttf")
+    })
+
+    const home = () => {
         props.navigation.navigate("home")
     }
+
+    const signup = () => {
+        props.navigation.navigate("signup")
+    }
+
+    useEffect(() => {
+        const backAction = () => {
+            BackHandler.exitApp();
+            return true;
+        }
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction,
+        );
+
+        return () => backHandler.remove();
+    }, []);
+
+    if(!loaded) {
+        return null
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Login</Text>
-            <Button title="signup" onPress={signUp}>Sign Up</Button>
-            <Button title="login" onPress={login}>Login</Button>
+        <View style={styles.container} >
+            <ImageBackground style={styles.image} source={ require("../../assets/beetle.png") }>
+                <StatusBar style="auto" />
+                <Text style={styles.text}>
+                    Iniciar
+                </Text>
+                <View style={styles.inputView} >
+                    <TextInput
+                        style={[styles.TextInput]}
+                        placeholder="Ingrese su correo"
+                        placeholderTextColor="#808080"
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.loginBtn} onPress={home}>
+                    <Text style={styles.textButton}>Continuar</Text>
+                </TouchableOpacity>
+
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>¿Aún no te has registrado? {' '}
+                        <Text style={ styles.underline } onPress={signup}>
+                            Regístrate aquí
+                        </Text>
+                    </Text>
+                </View>
+            </ImageBackground>
         </View>
     );
 }
 
+
 const styles = StyleSheet.create({
-    container : {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    container: {
+        flex: 1,
     },
-})
+
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    footerText: {
+        color: '#FFFF'
+    },
+
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        marginBottom: 30,
+    },
+
+    inputView: {
+        backgroundColor: "#FFFF",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "flex-start",
+    },
+
+    underline: {
+        textDecorationLine: 'underline',
+    },
+
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+        width: "100%",
+    },
+
+    textButton: {
+        color: "#FFFF",
+        fontSize: 20,
+    },
+
+    text: {
+        color: "#FFFF",
+        marginBottom: 10,
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontFamily: 'Helvetica',
+    },
+
+    loginBtn: {
+        width: "40%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: "#275DAD",
+    },
+});
+
 export default LoginScreen;
