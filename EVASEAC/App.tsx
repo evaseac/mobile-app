29ca67bd1817406ec60b4ab9d6a8c27af09a546c
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2023-04-09 21:52:26 +0200

    Linter (#23)
    * Add eslint and prettier to project
    
    * Add github workflows
    
    * Update github workflows build
    
    * Fix working directory in github workflows build
    
    * Fix node cache in github workflows build
    
    * Fix node version github workflows build
    
    * Add platform arg in github workflows build
    
    * Fix dependencies errors
    
    * Fix linter errors
    
    ---------
    
    Co-authored-by: DanielMontesGuerrero <danimontesguerrero17@gmail.com>

--- EVASEAC/App.tsx
+++ EVASEAC/App.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 import AppNavigator from './app/App.navigator';
 
 const App = () => {
-  return (
-    <AppNavigator/>
-  );
-}
+  return <AppNavigator />;
+};
 
 export default App;
--- EVASEAC/app/screens/protocol1/Protocol1Screen.tsx
+++ EVASEAC/app/screens/protocol1/Protocol1Screen.tsx
@@ -1,43 +1,38 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 interface Protocol1Props {
-    navigation : any;
+  navigation: any;
 }
 
-const Protocol1Screen = (props:Protocol1Props) => {
-    const home = () => {
-        props.navigation.navigate("home")
-    }
-    return (
-        <View style={styles.container}>
-            <Text>Verifique que se cuentan con los siguientes materiales</Text>
-            <FlatList
-                data={[
-                    {key : 'Objeto 1'},
-                    {key : 'Objeto 2'},
-                    {key : 'Objeto 3'},
-                ]}
-                renderItem={({item})=><Text style={styles.item}>{item.key}</Text>}
-            />
-        </View>
-    );
-
-}
+const Protocol1Screen = (props: Protocol1Props) => {
+  const home = () => {
+    props.navigation.navigate('home');
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Verifique que se cuentan con los siguientes materiales</Text>
+      <FlatList
+        data={[{key: 'Objeto 1'}, {key: 'Objeto 2'}, {key: 'Objeto 3'}]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: 40,
-    },
-    item : {
-        padding: 5,
-        fontSize: 15,
-        height: 30,
-        alignContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+  item: {
+    padding: 5,
+    fontSize: 15,
+    height: 30,
+    alignContent: 'center',
+  },
+});
 
 export default Protocol1Screen;
