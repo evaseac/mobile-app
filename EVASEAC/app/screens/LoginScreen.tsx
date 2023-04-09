dae6eb293112ab18987b98b23b6c9a9f04bea169
Esteban Raymundo Olmedo Ramírez <43709368+EstebanOlmedo@users.noreply.github.com>
2023-04-09 14:19:18 -0600

    Linter (#24)
    * Add eslint and prettier to project
    
    * Add github workflows
    
    * Update github workflows build
    
    * Fix working directory in github workflows build
    
    * Fix node cache in github workflows build
    
    * Fix node version github workflows build
    
    * Add platform arg in github workflows build
    
    * Fix dependencies errors
    
    * Fix linter errors
    
    ---------
    
    Co-authored-by: DanielMontesGuerrero <danimontesguerrero17@gmail.com>

--- EVASEAC/app/screens/LoginScreen.tsx
+++ EVASEAC/app/screens/LoginScreen.tsx
@@ -1,139 +1,148 @@
-import { TouchableOpacity, StyleSheet, Text, View, Image, TextInput, ImageBackground, BackHandler } from 'react-native';
-import { StatusBar } from 'expo-status-bar'
-import { useFonts } from 'expo-font'
-import { useEffect } from 'react'
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ImageBackground,
+  BackHandler,
+} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {useFonts} from 'expo-font';
+import {useEffect} from 'react';
 
 interface LoginScreenProps {
-    navigation : any;
+  navigation: any;
 }
 
-const LoginScreen = (props : LoginScreenProps) => {
-    const [loaded] = useFonts({
-        Helvetica : require("../../assets/fonts/Helvetica-Bold.ttf")
-    })
+const LoginScreen = (props: LoginScreenProps) => {
+  const [loaded] = useFonts({
+    Helvetica: require('../../assets/fonts/Helvetica-Bold.ttf'),
+  });
 
-    const home = () => {
-        props.navigation.navigate("home")
-    }
+  const home = () => {
+    props.navigation.navigate('home');
+  };
 
-    const signup = () => {
-        props.navigation.navigate("signup")
-    }
+  const signup = () => {
+    props.navigation.navigate('signup');
+  };
 
-    useEffect(() => {
-        const backAction = () => {
-            BackHandler.exitApp();
-            return true;
-        }
-        const backHandler = BackHandler.addEventListener(
-            'hardwareBackPress',
-            backAction,
-        );
-
-        return () => backHandler.remove();
-    }, []);
-
-    if(!loaded) {
-        return null
-    }
-
-    return (
-        <View style={styles.container} >
-            <ImageBackground style={styles.image} source={ require("../../assets/beetle.png") }>
-                <StatusBar style="auto" />
-                <Text style={styles.text}>
-                    Iniciar
-                </Text>
-                <View style={styles.inputView} >
-                    <TextInput
-                        style={[styles.TextInput]}
-                        placeholder="Ingrese su correo"
-                        placeholderTextColor="#808080"
-                    />
-                </View>
-
-                <TouchableOpacity style={styles.loginBtn} onPress={home}>
-                    <Text style={styles.textButton}>Continuar</Text>
-                </TouchableOpacity>
-
-                <View style={styles.footer}>
-                    <Text style={styles.footerText}>¿Aún no te has registrado? {' '}
-                        <Text style={ styles.underline } onPress={signup}>
-                            Regístrate aquí
-                        </Text>
-                    </Text>
-                </View>
-            </ImageBackground>
-        </View>
+  useEffect(() => {
+    const backAction = () => {
+      BackHandler.exitApp();
+      return true;
+    };
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
     );
-}
 
+    return () => backHandler.remove();
+  }, []);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={require('../../assets/beetle.png')}>
+        <StatusBar style="auto" />
+        <Text style={styles.text}>Iniciar</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={[styles.TextInput]}
+            placeholder="Ingrese su correo"
+            placeholderTextColor="#808080"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.loginBtn} onPress={home}>
+          <Text style={styles.textButton}>Continuar</Text>
+        </TouchableOpacity>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            ¿Aún no te has registrado?{' '}
+            <Text style={styles.underline} onPress={signup}>
+              Regístrate aquí
+            </Text>
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 
-    image: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    footerText: {
-        color: '#FFFF'
-    },
+  footerText: {
+    color: '#FFFF',
+  },
 
-    footer: {
-        position: 'absolute',
-        bottom: 0,
-        marginBottom: 30,
-    },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    marginBottom: 30,
+  },
 
-    inputView: {
-        backgroundColor: "#FFFF",
-        borderRadius: 30,
-        width: "70%",
-        height: 45,
-        marginBottom: 20,
-        alignItems: "flex-start",
-    },
+  inputView: {
+    backgroundColor: '#FFFF',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+    alignItems: 'flex-start',
+  },
 
-    underline: {
-        textDecorationLine: 'underline',
-    },
+  underline: {
+    textDecorationLine: 'underline',
+  },
 
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        marginLeft: 20,
-        width: "100%",
-    },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    width: '100%',
+  },
 
-    textButton: {
-        color: "#FFFF",
-        fontSize: 20,
-    },
+  textButton: {
+    color: '#FFFF',
+    fontSize: 20,
+  },
 
-    text: {
-        color: "#FFFF",
-        marginBottom: 10,
-        fontSize: 50,
-        fontWeight: 'bold',
-        fontFamily: 'Helvetica',
-    },
+  text: {
+    color: '#FFFF',
+    marginBottom: 10,
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'Helvetica',
+  },
 
-    loginBtn: {
-        width: "40%",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 40,
-        backgroundColor: "#275DAD",
-    },
+  loginBtn: {
+    width: '40%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    backgroundColor: '#275DAD',
+  },
 });
 
 export default LoginScreen;
--- EVASEAC/app/screens/protocol4/Protocol4Screen.tsx
+++ EVASEAC/app/screens/protocol4/Protocol4Screen.tsx
@@ -1,29 +1,28 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 interface Protocol4Props {
-    navigation : any;
+  navigation: any;
 }
 
-const Protocol4Screen = (props:Protocol4Props) => {
-    const home = () => {
-        props.navigation.navigate("home")
-    }
-    return (
-        <View style={styles.container}>
-            <Text>Hello world!</Text>
-        </View>
-    );
-
-}
+const Protocol4Screen = (props: Protocol4Props) => {
+  const home = () => {
+    props.navigation.navigate('home');
+  };
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: 40,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
 });
 
 export default Protocol4Screen;
